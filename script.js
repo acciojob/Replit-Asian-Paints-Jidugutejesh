bb5742f16ee5178446e7dae2132f0a690243eceb
Tejesh Jidugu <tejeshjidugu@gmail.com>
2025-08-28 22:16:44 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
 //your JS code here. If required.
+const changeButton = document.getElementById("change_button")
+const resetButton = document.getElementById("reset")
+
+changeButton.addEventListener("click",()=>changeColor())
+resetButton.addEventListener("click",()=>resetColor())
+function changeColor(){
+	resetColor()
+	
+	const selectedBlock = document.getElementById("block_id").value;
+	const assignedColor = document.getElementById("color_id").value;
+
+if(selectedBlock<1 || selectedBlock>9){
+	alert("Please enter valid block 1-9")
+	return
+}
+	
+	document.getElementById(selectedBlock).style.backgroundColor=assignedColor
+}
+function resetColor() {
+	for(let i=1;i<=9;i++){
+		document.getElementById(i.toString()).style.backgroundColor="transparent";
+	}
+	
+}
