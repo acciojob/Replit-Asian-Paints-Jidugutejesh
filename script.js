7d8e0a2ce8865e5344b53840a3ee0941df1c2a95
Tejesh Jidugu <tejeshjidugu@gmail.com>
2025-08-29 11:09:35 +0530

    created a submission

--- script.js
+++ script.js
@@ -11,10 +11,10 @@ function changeColor(){
 	const selectedBlock = document.getElementById("block_id").value;
 	const assignedColor = document.getElementById("color_id").value;
 
-//if(selectedBlock<1 || selectedBlock>9){
-//	alert("Please enter valid block 1-9")
-//	return
-//}
+if(selectedBlock<1 || selectedBlock>9){
+	alert("Please enter valid block 1-9")	
+		return
+}
 	
 	document.getElementById(selectedBlock).style.backgroundColor=assignedColor
 	     console.log(selectedBlock, assignedColor)
